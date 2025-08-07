3fa90a47966ac641e446ab3eaba3204182d8d183
Luis Davila <davilae107@gmail.com>
2025-08-06 21:43:10 -1200

    Update assembly information and add sourcelink support

--- obj/Debug/net6.0/SeleniumLoginAutomation.AssemblyInfo.cs
+++ obj/Debug/net6.0/SeleniumLoginAutomation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumLoginAutomation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+868f93c923cb2cda797e94ac3de6899431927bc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumLoginAutomation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumLoginAutomation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
